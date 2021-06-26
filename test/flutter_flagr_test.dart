dc1bfc65f181a893f2268b9236326677eecafc58
Risa Herdianto <aputra.zha@gmail.com>
2021-06-26 19:14:14 +0700

    Added Toggle Polling Timer

--- test/flutter_flagr_test.dart
+++ test/flutter_flagr_test.dart
@@ -4,7 +4,7 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  Flagr.init(api: "hallo");
+  Flagr.init("hallo");
 
   test('getPlatformVersion', () async {
     expect(Flagr.instance.url, 'hallo');
